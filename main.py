faefeea66c7a0e8c21edd37e5b3f617a2bfd1090
Sonu Patel <createwithsonu@gmail.com>
2026-07-07 22:45:18 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
 app=FastAPI()
+
+#home route
 @app.get("/")
 def home():
-    return {"message": "This is the home page of the FastAPI application."}
+    return {"message": "This is the home page."}
+
+#About route
+@app.get("/about")
+def about():
+    return {"message": "This is the about page."}
+
+#User route
+@app.get("/users/{user_id}")
+def users(user_id:int):
+    return {
+        "user_id":user_id
+    }
