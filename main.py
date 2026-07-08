03b454c58719f5692a986af844f72e97ef789a23
Sonu Patel <createwithsonu@gmail.com>
2026-07-08 16:58:23 +0530

    Query Parameter Route

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
 app=FastAPI()
 
-#home route
-@app.get("/")
-def home():
-    return {"message": "This is the home page."}
-
-#About route
-@app.get("/about")
-def about():
-    return {"message": "This is the about page."}
-
-#User route
-@app.get("/users/{user_id}")
-def users(user_id:int):
+@app.get("/users")
+def get_users(name: str = None):
     return {
-        "user_id":user_id
+        "Name": name
     }
