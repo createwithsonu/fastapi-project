2e2854ab325b37f50fd3127b186bbcffa8747873
Sonu Patel <createwithsonu@gmail.com>
2026-07-08 22:14:21 +0530

    Using POST Method

--- main.py
+++ main.py
@@ -22,3 +22,11 @@ def create_job(job_id:int=0, job_name:str=None):
         "job_id":job_id,
         "job_name":job_name,
     }
+
+#handling request body using Dictionary 
+@app.post("/newjobs")
+def new_jobs(jobs:dict):
+    return{
+        "Message":"Job created successfully",
+        "Hiring":jobs
+    }
