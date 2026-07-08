32324ab55cf84734562c077bc160fa319fa7c918
Sonu Patel <createwithsonu@gmail.com>
2026-07-08 19:51:05 +0530

    Path Para and Query Para

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 app=FastAPI()
 
-@app.get("/users")
-def get_users(name: str = None):
-    return {
-        "Name": name
+#Path Parameters
+@app.get("/jobs/{job_id}")
+def get_job(job_id:int):
+    return{
+        "job_id":job_id,
+    }
+
+#Query Parameters
+@app.get("/jobs")
+def get_jobs(job_id:int=None, job_name:str=None):
+    return{
+        "job_id":job_id,
+        "job_name":job_name,
     }
