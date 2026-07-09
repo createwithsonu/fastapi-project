5e40e1baee9fbd666722215c28d621611394c7d2
Sonu Patel <createwithsonu@gmail.com>
2026-07-09 14:40:23 +0530

    Tried Pydantic

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 from fastapi import FastAPI
+#Using Pydantic
+from pydantic import BaseModel
 app=FastAPI()
-
-#Path Parameters
-@app.get("/jobs/{job_id}")
-def get_job(job_id:int):
+class User(BaseModel):
+    name:str
+    age:int
+@app.post("/create-user")
+def create_user(user:User):
     return{
-        "job_id":job_id,
-    }
-
-#Query Parameters
-@app.get("/jobs")
-def get_jobs(job_id:int=0, job_name:str=None):
-    return{
-        "job_id":job_id,
-        "job_name":job_name,
-    }
-
-@app.post("/create-job")
-def create_job(job_id:int=0, job_name:str=None):
-    return{
-        "job_id":job_id,
-        "job_name":job_name,
-    }
-
-#handling request body using Dictionary 
-@app.post("/newjobs")
-def new_jobs(jobs:dict):
-    return{
-        "Message":"Job created successfully",
-        "Hiring":jobs
+        "Message":"User Created Successfully !",
+        "Data":user
     }
