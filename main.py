d27b8a7f8a948d40bc6718e1bcc0a46a78daba9d
Sonu Patel <createwithsonu@gmail.com>
2026-07-09 16:22:25 +0530

    Create , Read Done

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI
-#Using Pydantic
 from pydantic import BaseModel
 
 app = FastAPI()
 
-#Handling Nested JSON data using Pydantic models
-class Adress(BaseModel):
-    city: str
-    state: str
-    pincode: int
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
-    address: Adress
-@app.post("/create-User")
-def create_user(user:User):
-    return{
-        "message": "User created successfully !",
-        "user": user
-    }
+todos = []
+
+class Todo(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
+@app.post("/todo")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo created successfully", "todo": todo}
+
+@app.get("/todo")
+def get_todos():
+    return todos
+
+#for fetching a specific todo by id using path parameter
+@app.get("/todo/{todo_id}")
+def get_todo_by_id(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"message": "Todo not found"}
