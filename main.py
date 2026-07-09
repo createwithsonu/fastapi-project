bb257dd1adab776e86101d8d7dd9345bc88dbf74
Sonu Patel <createwithsonu@gmail.com>
2026-07-09 15:19:40 +0530

    Handling Nested JSON data

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#Handling Nested JSON data using Pydantic models
+class Adress(BaseModel):
+    city: str
+    state: str
+    pincode: int
 class User(BaseModel):
     name: str
     age: int
     email: str
-@app.post("/users")
+    address: Adress
+@app.post("/create-User")
 def create_user(user:User):
     return{
         "message": "User created successfully !",
