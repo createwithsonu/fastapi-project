b43741dfa5351304c7f1f595419821102bdf459e
Sonu Patel <createwithsonu@gmail.com>
2026-07-10 23:53:14 +0530

    Implemented Path - Query - Body Together

--- main.py
+++ main.py
@@ -3,45 +3,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-todos = []
+users = []
 
-class Todo(BaseModel):
-    id: int
-    title: str
-    completed: bool
+class User(BaseModel):
+    name: str
+    age: int
 
-@app.post("/todo")
-def create_todo(todo: Todo):
-    todos.append(todo)
-    return {"message": "Todo created successfully", "todo": todo}
+@app.post("/users")
+def create_user(user: User):
+    users.append(user)
+    return{
+"message": "User created successfully", "user": user
+    }
 
-@app.get("/todo")
-def get_todos():
-    return todos
-
-#for fetching a specific todo by id using path parameter
-@app.get("/todo/{todo_id}")
-def get_todo_by_id(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return todo
-    return {"message": "Todo not found"}
-
-#Updating a specific todo by id using path parameter
-@app.put("/todo/{todo_id}")
-def update_todo(todo_id: int, updated_todo: Todo):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos[index] = updated_todo
-            return {"message": "Todo updated successfully", "todo": updated_todo}
-    return {"message": "Todo not found"}
-
-
-#Deleting a specific todo by id using path parameter
-@app.delete("/todo/{todo_id}")
-def delete_todo(todo_id: int):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            deleted_todo = todos.pop(index)
-            return {"message": "Todo deleted successfully", "todo": deleted_todo}
-    return {"message": "Todo not found"}
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User, notify: bool = False):
+#indexing starting from 0 for user_id
+    if user_id < len(users):
+        users[user_id] = user
+        return{
+            "Message": "User updated successfully", "data": user, "notify": notify
+        }
+    return{
+        "Error": "User not found"
+    }
